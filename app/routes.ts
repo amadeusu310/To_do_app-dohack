4724f47600556919d73b3795ebdf273b0d023706
saya_38_t <sayabear30@gmail.com>
2025-08-06 21:57:44 +0900

    ページ追加

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,12 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/about", "routes/about.tsx"),
+  route("/projectcheck", "routes/projectcheck.tsx"),
+  route("/projectregister", "routes/projectregister.tsx"),
+  route("/calender", "routes/calender.tsx"),
+  route("/taskregister", "routes/taskregister.tsx"),
+  route("/taskCheck", "routes/taskCheck.tsx"),
+  route("/login", "routes/login.tsx"),
+  route("/ranking", "routes/ranking.tsx"),
+  route("/register", "routes/register.tsx")
 ] satisfies RouteConfig;
